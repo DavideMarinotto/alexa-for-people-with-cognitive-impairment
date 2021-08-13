519c03693bf153b79be6b99ce9306194de5c20c2
MazzaAlessandro <10670112@polimi.it>
2021-08-13 11:18:18 +0200

    helmet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet =require('helmet');
 
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin.routes');
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
