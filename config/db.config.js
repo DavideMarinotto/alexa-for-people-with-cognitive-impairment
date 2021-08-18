1bfd6e3c4a63d50e982b4b48406e7c0d8ba1e432
Davide Marinotto <davide.marinotto@mail.polimi.it>
2021-08-18 12:31:05 +0200

    db data

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "TIWuser",
-    PASSWORD: "TIWpass",
-    DB: "proginginf"
+    HOST: "eu-cdbr-west-01.cleardb.com",
+    USER: "b535b24bf462e4",
+    PASSWORD: "8679a566",
+    DB: "heroku_a59747653413ae6"
 };
